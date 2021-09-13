87eb783b6ae87d794d34d201435583a29045880d
Bidyashish Kumar <bidyashishk@gmail.com>
2021-09-13 12:45:29 +0800

    Update add Git commit

--- git-alias.sh
+++ git-alias.sh
@@ -5,7 +5,8 @@ echo 'alias gb="git branch"' >> ~/.bashrc
 echo 'alias gp="git pull"' >> ~/.bashrc
 echo 'alias gps="git push"' >> ~/.bashrc
 echo 'alias gm="git merge"' >> ~/.bashrc
-echo 'alias ges="git log --all --grep="' >> ~/.bashrc
+echo 'alias gl="git log --all --grep="' >> ~/.bashrc
 echo 'alias gs="git status"' >> ~/.bashrc
 echo 'alias gd="git diff --name-only"' >> ~/.bashrc
+echo 'alias ga="git add . && git commit -m"' >> ~/.bashrc
 echo 'alias sync="browser-sync start --server --files --directory -w  ./"' >> ~/.bashrc
